4740f85b80563e112f6646ef48d3c15443d4e4b1
Rijndael <rijndael@baldy.ga>
2024-12-12 20:31:23 +0000

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SocketIOStatus from "./SocketIOStatus";
 import { Container, Paper, Stack } from "@mui/material";
 
 export default function Home() {
-    return <Container maxWidth="lg" >
+    return <Container maxWidth="lg" sx={{margin: "1em auto"}}>
         <Stack gap={1}>
             <Paper>
                 <Container maxWidth="md">
